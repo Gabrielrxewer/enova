a5b99d0ecc13456aab60027354f6d95584a0437e
Gabriel Roewer <dev.gaabriel@gmail.com>
2023-02-22 17:07:10 -0300

    Identação - Impressão PDF's

--- programas-enova/imprimir.py
+++ programas-enova/imprimir.py
@@ -12,11 +12,15 @@ root.title("Unir PDF's")
 dir_path = StringVar()
 
 # Define a função que atualiza o caminho do diretório com o que o usuário digitou
+
+
 def update_dir_path():
     new_dir_path = filedialog.askdirectory()
     dir_path.set(new_dir_path)
 
 # Define a função que une os PDFs e imprime o PDF unificado
+
+
 def merge_and_print():
     # Inicializa o objeto PdfFileMerger
     merged_pdf = PdfMerger()
@@ -37,8 +41,10 @@ def merge_and_print():
     # Imprime o PDF unificado
     win32api.ShellExecute(0, "print", output_path, None, ".", 0)
 
+
 def cancel_button():
     root.destroy()
+
 
 # Cria o widget do input para o caminho do diretório
 dir_path_label = Label(root, text="Caminho do diretório:")
@@ -47,11 +53,13 @@ dir_path_entry = Entry(root, textvariable=dir_path, width=80)
 dir_path_entry.pack()
 
 # Cria o botão para selecionar um novo diretório
-dir_path_button = Button(root, text="Selecionar diretório", command=update_dir_path)
+dir_path_button = Button(
+    root, text="Selecionar diretório", command=update_dir_path)
 dir_path_button.pack()
 
 # Cria o botão para unir os PDFs e imprimir o PDF unificado
-merge_and_print_button = Button(root, text="Unir PDFs e imprimir", command=merge_and_print)
+merge_and_print_button = Button(
+    root, text="Unir PDFs e imprimir", command=merge_and_print)
 merge_and_print_button.pack()
 
 cancel_button = Button(root, text="Cancelar", command=cancel_button)
