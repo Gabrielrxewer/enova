876ec54d8d749523bf7b06b33820b5b85a26ffb4
Gabriel Roewer <dev.gaabriel@gmail.com>
2023-02-27 09:51:22 -0300

    Testing - excel converting in db

---
+++ programas-enova/excelindb.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import sqlite3
+
+# Lê o arquivo Excel e transforma em um dataframe
+df = pd.read_excel('C:/Users/inspe/Desktop/Qualidade/Projetos py/programas-enova/astec.xlsx', sheet_name='1')
+
+# Conecta com o banco de dados SQLite
+conn = sqlite3.connect('astec.db')
+
+# Cria a tabela no banco de dados
+df.to_sql('asistec', conn, if_exists='replace', index=False)
+
+# Fecha a conexão com o banco de dados
+conn.close()
