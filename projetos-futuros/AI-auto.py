177b53ed0b2fac2c33f5e467e7ba86148137ebe0
Gabrielrxewer <roewergabriel@gmail.com>
2023-03-10 11:01:28 -0300

    IA - Testes

--- projetos-futuros/AI-auto.py
+++ projetos-futuros/AI-auto.py
@@ -1,2 +1,11 @@
+import mysql.connector
+import tensorflow as tf
 
+mydb = mysql.connector.connect(
+  host="endereço da host",
+  user="usuario da host",
+  password="senha do usuario",
+  database="nome do banco de dados"
+)
 
+mycursor = mydb.cursor()
